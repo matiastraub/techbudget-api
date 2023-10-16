1bc39e964d417ef094a0f330c09a1b6fba589e4d
matiastraub <matias.traub@gmail.com>
2023-10-16 17:01:46 -0400

    Fix eslint and upload transaction

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,22 +1,22 @@
-const ErrorResponse = require("../utils/ErrorResponse");
-const asyncHandler = require("../middleware/async");
-const Transaction = require("../models/Transaction");
-const Category = require("../models/Category");
+const ErrorResponse = require('../utils/ErrorResponse')
+const asyncHandler = require('../middleware/async')
+const Transaction = require('../models/Transaction')
+const Category = require('../models/Category')
 
 const getUniqueCategories = (reqTransactions) => {
-  const categories = [];
+  const categories = []
   reqTransactions.forEach((item) => {
-    categories.push(item.category);
-  });
-  return [...new Set(categories)];
-};
+    categories.push(item.category)
+  })
+  return [...new Set(categories)]
+}
 
 // @desc    Get all transactions
 // @route   GET /api/v1/transactions
 // @access  Private
 exports.getTransactions = asyncHandler(async (req, res) => {
-  res.status(200).json(res.advancedQueries);
-});
+  res.status(200).json(res.advancedQueries)
+})
 
 // @desc    Get a given transaction
 // @route   GET /api/v1/transactions/:id
@@ -25,61 +25,68 @@ exports.getTransaction = asyncHandler(async (req, res, next) => {
   const transaction = await Transaction.findById(
     { user: req.user.id },
     req.params.id
-  );
+  )
   if (!transaction) {
-    next(new ErrorResponse(`Not found with id: ${req.params.id}`, 404));
+    next(new ErrorResponse(`Not found with id: ${req.params.id}`, 404))
   }
-  res.status(200).json({ sucess: true, data: transaction });
-});
+  res.status(200).json({ sucess: true, data: transaction })
+})
 
 // @desc    Create a transaction
 // @route   POST /api/v1/transactions
 // @access  Private
 exports.createTransaction = asyncHandler(async (req, res) => {
-  req.body.user = req.user.id;
+  req.body.user = req.user.id
   const category = await Category.findOne({
     user: req.body.user,
     name: req.body.category,
-  });
-  req.body.category = category.id;
-  const transaction = await Transaction.create(req.body);
-  return res.status(201).json({ sucess: true, data: transaction });
-});
+  })
+  req.body.category = category.id
+  const transaction = await Transaction.create(req.body)
+  return res.status(201).json({ sucess: true, data: transaction })
+})
 
 // @desc    Upload a transaction file (Excel from the front-end)
 // @route   POST /api/v1/transactions/upload
 // @access  Private
 exports.uploadTransactionFile = asyncHandler(async (req, res, next) => {
-  const categories = getUniqueCategories(req.body);
-  const categoryResp = await Category.find({ user: req.user.id });
-  const mappedCategories = categoryResp.map((categ) => categ.name);
+  const categories = getUniqueCategories(req.body)
+  const categoryResp = await Category.find({ user: req.user.id })
+  const mappedCategories = categoryResp.map((categ) => categ.name)
 
-  const difference = categories.filter((x) => !mappedCategories.includes(x));
+  const difference = categories.filter((x) => !mappedCategories.includes(x))
 
   if (difference.length === 1) {
     return next(
       new ErrorResponse(`this category does not exist ${difference[0]}`, 500)
-    );
+    )
   }
   if (difference.length) {
     return next(
       new ErrorResponse(
-        `These categories do not exist ${difference.join(",")}`,
+        `These categories do not exist ${difference.join(',')}`,
         500
       )
-    );
+    )
+  }
+
+  // Check max size
+  if (req.body.length > process.env.MAX_TRANSACTIONS_UPLOAD) {
+    return next(
+      new ErrorResponse(`Upload limit reached ${req.body.length}`, 400)
+    )
   }
 
   req.body.forEach((element) => {
     element.category = categoryResp.find(
       (cat) => cat.name === element.category
-    ).id;
-    element.user = req.user.id;
-  });
+    ).id
+    element.user = req.user.id
+  })
 
-  const transactions = await Transaction.insertMany(req.body);
-  return res.status(201).json({ sucess: true, data: transactions });
-});
+  const transactions = await Transaction.insertMany(req.body)
+  return res.status(201).json({ sucess: true, data: transactions })
+})
 
 // @desc    Update a given transaction
 // @route   PUT /api/v1/transactions/:id
@@ -92,20 +99,20 @@ exports.updateTransaction = asyncHandler(async (req, res, next) => {
       new: true,
       runValidators: true,
     }
-  );
+  )
   if (!transaction) {
-    next(new ErrorResponse(`Not found with id: ${req.params.id}`, 404));
+    next(new ErrorResponse(`Not found with id: ${req.params.id}`, 404))
   }
-  res.status(200).json({ sucess: true, data: transaction });
-});
+  res.status(200).json({ sucess: true, data: transaction })
+})
 
 // @desc    Delete a given transaction
 // @route   DELETE /api/v1/transactions/:id
 // @access  Private
 exports.deleteTransaction = asyncHandler(async (req, res, next) => {
-  const transaction = await Transaction.findByIdAndDelete(req.params.id);
+  const transaction = await Transaction.findByIdAndDelete(req.params.id)
   if (!transaction) {
-    next(new ErrorResponse(`Not found with id: ${req.params.id}`, 404));
+    next(new ErrorResponse(`Not found with id: ${req.params.id}`, 404))
   }
-  res.status(200).json({ sucess: true, data: {} });
-});
+  res.status(200).json({ sucess: true, data: {} })
+})
--- controllers/users.js
+++ controllers/users.js
@@ -1,19 +1,19 @@
-const path = require("path");
-const fs = require("fs");
-const ErrorResponse = require("../utils/ErrorResponse");
-const asyncHandler = require("../middleware/async");
-const User = require("../models/User");
-const geocoder = require("../utils/geocoder");
-const { isDir } = require("../utils/file");
-const config = require("../config/config");
+const path = require('path')
+const fs = require('fs')
+const ErrorResponse = require('../utils/ErrorResponse')
+const asyncHandler = require('../middleware/async')
+const User = require('../models/User')
+const geocoder = require('../utils/geocoder')
+const { isDir } = require('../utils/file')
+const config = require('../config/config')
 
 // @desc    Get all Users
 // @route   GET /api/v1/users
 // @access  Private
 exports.getUsers = asyncHandler(async (req, res) => {
-  const users = await User.find(req.query);
-  res.status(200).json({ sucess: true, count: users.length, data: users });
-});
+  const users = await User.find(req.query)
+  res.status(200).json({ sucess: true, count: users.length, data: users })
+})
 
 // @desc    Update user details
 // @route   PUT /api/v1/user
@@ -22,32 +22,32 @@ exports.updateDetails = asyncHandler(async (req, res) => {
   const fieldsToUpdate = {
     name: req.body.name,
     email: req.body.email,
-  };
+  }
   const user = await User.findOneAndUpdate(req.user.id, fieldsToUpdate, {
     new: true,
     runValidators: true,
-  });
-  res.status(200).json({ sucess: true, data: user });
-});
+  })
+  res.status(200).json({ sucess: true, data: user })
+})
 
 // @desc    Get all Users
 // @route   GET /api/v1/Users/:id
 // @access  Private
 exports.getUser = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.params.id);
+  const user = await User.findById(req.params.id)
   if (!user) {
-    return next(new ErrorResponse(`Not found with id: ${req.params.id}`, 404));
+    return next(new ErrorResponse(`Not found with id: ${req.params.id}`, 404))
   }
-  return res.status(200).json({ sucess: true, data: user });
-});
+  return res.status(200).json({ sucess: true, data: user })
+})
 
 // @desc    Create a User
 // @route   POST /api/v1/Users
 // @access  Private
 exports.createUser = asyncHandler(async (req, res) => {
-  const user = await User.create(req.body);
-  res.status(201).json({ sucess: true, data: user });
-});
+  const user = await User.create(req.body)
+  res.status(201).json({ sucess: true, data: user })
+})
 
 // @desc    Update a given User
 // @route   PUT /api/v1/users/:id
@@ -56,95 +56,95 @@ exports.updateUser = asyncHandler(async (req, res, next) => {
   const user = await User.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
-  });
+  })
   if (!user) {
-    return next(new ErrorResponse(`Not found with id: ${req.params.id}`, 404));
+    return next(new ErrorResponse(`Not found with id: ${req.params.id}`, 404))
   }
-  return res.status(200).json({ sucess: true, data: user });
-});
+  return res.status(200).json({ sucess: true, data: user })
+})
 
 // @desc    Delete a given User
 // @route   DELETE /api/v1/users/:id
 // @access  Private
 exports.deleteUser = asyncHandler(async (req, res, next) => {
-  const user = await User.findByIdAndDelete(req.params.id);
+  const user = await User.findByIdAndDelete(req.params.id)
   if (!user) {
-    return next(new ErrorResponse(`Not found with id: ${req.params.id}`, 404));
+    return next(new ErrorResponse(`Not found with id: ${req.params.id}`, 404))
   }
-  return res.status(200).json({ sucess: true, data: {} });
-});
+  return res.status(200).json({ sucess: true, data: {} })
+})
 
 // @desc    Get users within a radius
 // @route   GET /api/v1/users/:zipcode/:distance
 // @access  Private
 exports.getUsersByRadius = asyncHandler(async (req, res, next) => {
-  const { zipcode, distance } = req.params;
-  const loc = await geocoder.geocode(zipcode);
-  const lat = loc[0].latitude;
-  const long = loc[0].longitude;
+  const { zipcode, distance } = req.params
+  const loc = await geocoder.geocode(zipcode)
+  const lat = loc[0].latitude
+  const long = loc[0].longitude
 
-  const geoKm = config.earthRadius.km;
-  const radius = distance / geoKm;
+  const geoKm = config.earthRadius.km
+  const radius = distance / geoKm
 
   // eslint-disable-next-line max-len
   const user = await User.find({
     location: { $geoWithin: { $centerSphere: [[long, lat], radius] } },
-  });
+  })
 
   if (!user) {
-    return next(new ErrorResponse(`Not found with id: ${req.params.id}`, 404));
+    return next(new ErrorResponse(`Not found with id: ${req.params.id}`, 404))
   }
-  return res.status(200).json({ sucess: true, count: user.length, data: user });
-});
+  return res.status(200).json({ sucess: true, count: user.length, data: user })
+})
 
 // @desc    Upload picture for a given user
 // @route   PUT /api/v1/users/:id/photo
 // @access  Private
 exports.userPhotoUpload = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.params.id);
+  const user = await User.findById(req.params.id)
   if (!user) {
     return next(
       new ErrorResponse(`User not found with id: ${req.params.id}`, 404)
-    );
+    )
   }
 
-  const uploadPath = `${process.env.FILE_UPLOAD_PATH}/${req.params.id}`;
+  const uploadPath = `${process.env.FILE_UPLOAD_PATH}/${req.params.id}`
 
   // Create folder if it does not exist
   if (!fs.existsSync(uploadPath)) {
-    fs.mkdirSync(uploadPath);
+    fs.mkdirSync(uploadPath)
   }
 
   if (!req.files) {
-    return next(new ErrorResponse("Please upload a file", 400));
+    return next(new ErrorResponse('Please upload a file', 400))
   }
   // return next(new ErrorResponse('Exiting process for testing', 400));
-  const { file } = req.files;
+  const { file } = req.files
   // return res.status(200).json({ sucess: true, data: file });
   // Check whether image is a photo with valid extension
-  if (!file.mimetype.startsWith("image")) {
-    return next(new ErrorResponse("Please upload an image file", 400));
+  if (!file.mimetype.startsWith('image')) {
+    return next(new ErrorResponse('Please upload an image file', 400))
   }
 
   // Check max size
   if (file.size > process.env.MAX_FILE_UPLOAD) {
-    return next(new ErrorResponse("Upload limit reached", 400));
+    return next(new ErrorResponse('Upload limit reached', 400))
   }
 
-  const { ext } = path.parse(file.name);
-  const timestamp = new Date().getTime();
+  const { ext } = path.parse(file.name)
+  const timestamp = new Date().getTime()
   // Rename file
-  file.name = file.name.replace(/.(jpeg|jpg|png|gif|webp)/g, "");
-  file.name = `${file.name}_${timestamp}${ext}`;
+  file.name = file.name.replace(/.(jpeg|jpg|png|gif|webp)/g, '')
+  file.name = `${file.name}_${timestamp}${ext}`
 
   file.mv(`${uploadPath}/${file.name}`, async (err) => {
     if (err) {
-      next(new ErrorResponse(`Problem with file ${err}`, 500));
+      next(new ErrorResponse(`Problem with file ${err}`, 500))
     }
-    await User.findByIdAndUpdate(req.params.id, { photo: file.name });
-  });
-  return res.status(200).json({ sucess: true, data: file.name });
-});
+    await User.findByIdAndUpdate(req.params.id, { photo: file.name })
+  })
+  return res.status(200).json({ sucess: true, data: file.name })
+})
 
 // @desc    Get all the files for a given user
 // @route   PUT /api/v1/users/:id/files
@@ -153,32 +153,32 @@ exports.getAllFiles = asyncHandler(async (req, res, next) => {
   if (req.params.id !== req.user.id) {
     next(
       new ErrorResponse(
-        "User not allowed to retrieve files from directory",
+        'User not allowed to retrieve files from directory',
         500
       )
-    );
+    )
   }
 
-  const album = req.params.album ? `/${req.params.album}` : "";
+  const album = req.params.album ? `/${req.params.album}` : ''
 
   const uploadPath = path.join(
     __dirname,
     `../public/uploads/${req.params.id}${album}`
-  );
+  )
   fs.readdir(uploadPath, async (err, files) => {
     if (err) {
-      next(new ErrorResponse("Unable to scan directory", 500));
+      next(new ErrorResponse('Unable to scan directory', 500))
     }
-    const doc = [];
+    const doc = []
     files.forEach((file) => {
-      const isFolder = isDir(fs, `${uploadPath}/${file}`);
+      const isFolder = isDir(fs, `${uploadPath}/${file}`)
       const obj = {
         file,
         name: file,
         isFolder,
-      };
-      doc.push(obj);
-    });
-    res.status(200).json({ sucess: true, count: doc.length, data: doc });
-  });
-});
+      }
+      doc.push(obj)
+    })
+    res.status(200).json({ sucess: true, count: doc.length, data: doc })
+  })
+})
