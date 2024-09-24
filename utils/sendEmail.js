29d1242b6aaa87d41f5f91ce052ad4a8431b8cd4
Matias Traub <matias.traub@gmail.com>
2024-09-24 20:38:27 +0200

    Lint format

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,12 +1,10 @@
-
-const emailAuth = require('../config/configEnv');
-const nodemailer = require("nodemailer");
-const config = require('../config/config');
+const emailAuth = require('../config/configEnv')
+const nodemailer = require('nodemailer')
+const config = require('../config/config')
 const fs = require('fs')
 const axios = require('axios')
 
-
-const {host, port, auth} = emailAuth
+const { host, port, auth } = emailAuth
 const transporter = nodemailer.createTransport({
   host,
   port,
@@ -20,43 +18,93 @@ const transporter = nodemailer.createTransport({
   //   user: process.env.SMTP_EMAIL,
   //   pass: process.env.SMTP_PASSWORD,
   // },
-});
-
+})
 
 /**
  * Check the default email sender set on the config
- * 
- * @param {String} defaultSender 
+ *
+ * @param {String} defaultSender
  * @returns {Function | String}
  */
 const emailSender = (defaultSender) => {
-  if(defaultSender === 'gogogol') {
+  if (defaultSender === 'gogogol') {
     return sendGGGEmail
   }
   //Test email
-  return sendEmail 
+  return sendEmail
 }
 
-exports.sendVerifyEmailWithTokenEmail = async (req,res,next,user,token,defaultSender) => {
-  await sendEmailTransportMessage(req,res,next,user,token,'verificationEmail','Welcome to TechBudget',{},emailSender(defaultSender));
+exports.sendVerifyEmailWithTokenEmail = async (
+  req,
+  res,
+  next,
+  user,
+  token,
+  defaultSender
+) => {
+  await sendEmailTransportMessage(
+    req,
+    res,
+    next,
+    user,
+    token,
+    'verificationEmail',
+    'Welcome to TechBudget',
+    {},
+    emailSender(defaultSender)
+  )
 }
 
-exports.sendForgotPasswordEmail =  async (req, res, next, user, token,defaultSender) => {
-  await sendEmailTransportMessage(req,res,next,user,token,'forgotPasswordEmail','Forgot Password',{resettoken: token,email:user.email},emailSender(defaultSender))
+exports.sendForgotPasswordEmail = async (
+  req,
+  res,
+  next,
+  user,
+  token,
+  defaultSender
+) => {
+  await sendEmailTransportMessage(
+    req,
+    res,
+    next,
+    user,
+    token,
+    'forgotPasswordEmail',
+    'Forgot Password',
+    { resettoken: token, email: user.email },
+    emailSender(defaultSender)
+  )
   next()
 }
 
-exports.sendResendTokenEmail = async (req, res, next, user, token,defaultSender) => {
-  await sendEmailTransportMessage(req,res,next,user,token,'resendTokenEmail','Verify Email',{},emailSender(defaultSender))
+exports.sendResendTokenEmail = async (
+  req,
+  res,
+  next,
+  user,
+  token,
+  defaultSender
+) => {
+  await sendEmailTransportMessage(
+    req,
+    res,
+    next,
+    user,
+    token,
+    'resendTokenEmail',
+    'Verify Email',
+    {},
+    emailSender(defaultSender)
+  )
 }
 
 /**
  * Send Email
- * 
- * @param {Object} options 
- * @param {*} next 
+ *
+ * @param {Object} options
+ * @param {*} next
  */
-const sendEmail = async (options,next) => {
+const sendEmail = async (options, next) => {
   // send mail with defined transport object
   const message = {
     from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
@@ -64,29 +112,29 @@ const sendEmail = async (options,next) => {
     subject: options.subject,
     text: options.message,
     html: `${options.message}`,
-  };
+  }
   try {
-    await transporter.sendMail(message);
+    await transporter.sendMail(message)
     next()
-  } catch(error) {
+  } catch (error) {
     console.error('Server email failed: ', error)
     next(new ErrorResponse('Server email failed', 500))
   }
-};
+}
 
 /**
- * Send GGG emails 
- * @param {Object} options 
- * @param {*} next 
+ * Send GGG emails
+ * @param {Object} options
+ * @param {*} next
  */
-const sendGGGEmail = async (options,next) => {
+const sendGGGEmail = async (options, next) => {
   const url = config.apiGogogol.sendEmail
   const postData = {
     username: process.env.GGG_USERNAME,
     pass: process.env.GGG_PASS,
     email: options.email,
     subject: options.subject,
-    msg: options.message
+    msg: options.message,
   }
   try {
     const resp = await axios.post(url, postData, {
@@ -97,12 +145,10 @@ const sendGGGEmail = async (options,next) => {
       console.log('resp sendGGG Email: ', resp.data)
       next()
     }
-  } catch(error) {
+  } catch (error) {
     next(new ErrorResponse('GGG Server email failed', 500))
   }
 }
-
-
 
 const sendEmailTransportMessage = async (
   req,
@@ -115,21 +161,23 @@ const sendEmailTransportMessage = async (
   params = {},
   fn
 ) => {
-
   const basicUrl = getUrl()
-  const paramUrl = '?'+(Object.keys(params).length ? objectToQueryString(params) :  `token=${token}`)
- 
+  const paramUrl =
+    '?' +
+    (Object.keys(params).length
+      ? objectToQueryString(params)
+      : `token=${token}`)
+
   const url = `${basicUrl}/${paramUrl}`
   const message = getEmailTemplate(req, user, template, url)
-  
+
   try {
-    await fn({email: user.email,subject,message}, next)
+    await fn({ email: user.email, subject, message }, next)
   } catch (error) {
     await user.save({ validateBeforeSave: false })
     next(new ErrorResponse('Email could not be sent', 500))
   }
 }
-
 
 const getUrl = () => {
   const isProd = process.env.NODE_ENV === 'production'
@@ -139,11 +187,10 @@ const getUrl = () => {
 
 const objectToQueryString = (obj) => {
   const queryString = Object.keys(obj)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`)
-    .join('&');
-  return queryString;
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`)
+    .join('&')
+  return queryString
 }
-
 
 /**
  * Get Email template
@@ -161,4 +208,3 @@ const getEmailTemplate = (req, user, templateName, link) => {
     .replace('{name}', user.name)
     .replace('{logo}', logoUrl)
 }
-
