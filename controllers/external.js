b0f038b5553169c7fa3a10e388763879b1a27ccb
Matias Traub <matias.traub@gmail.com>
2025-02-28 12:44:13 +0100

    Add external crypto history controller and route

--- controllers/external.js
+++ controllers/external.js
@@ -1,12 +1,16 @@
 const ErrorResponse = require('../utils/ErrorResponse')
 const asyncHandler = require('../middleware/async')
 const axios = require('axios')
-const apiCrypto = require('../data/mock/apiCrypto.json')
+const apiCrypto = require('../data/mock/crypto/apiCrypto.json')
+const apiCryptoHistory = require('../data/mock/crypto/apiCryptoHistory.json')
+
+//Crypto
 
 exports.getCrypto = asyncHandler(async (req, res) => {
+  const url = `${process.env.CRYPTO_RAPID_API_URL}/coins`
   const options = {
     method: 'GET',
-    url: process.env.CRYPTO_RAPID_API_URL,
+    url,
     params: {
       limit: process.env.CRYPTO_RAPID_API_LIMIT,
       timePeriod: process.env.CRYPTO_RAPID_TIME_PERIOD,
@@ -17,12 +21,39 @@ exports.getCrypto = asyncHandler(async (req, res) => {
       'X-RapidAPI-Host': process.env.CRYPTO_RAPID_API_HOST,
     },
   }
-
   const mockData = process.env.CRYPTO_RAPID_MOCK_API_DATA === 'true'
   const resp = mockData ? apiCrypto : await axios.request(options)
   const serverCoins = mockData ? resp.data : resp?.data?.data?.coins
   res.status(200).json({ success: true, data: serverCoins })
 })
+
+exports.getCryptoHistory = asyncHandler(async (req, res) => {
+  const defaultTime =
+    req.params.time || process.env.CRYPTO_RAPID_TIME_PERIOD_HISTORY
+  const url = `${process.env.CRYPTO_RAPID_API_URL}/coin/${process.env.CRYPTO_RAPID_DEFAULT_COIN_ID}/history`
+  const options = {
+    method: 'GET',
+    url,
+    params: {
+      referenceCurrencyUuid:
+        process.env.CRYPTO_RAPID_DEFAULT_REFERENCE_CURRENCY,
+      limit: process.env.CRYPTO_RAPID_API_LIMIT,
+      timePeriod: defaultTime,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+      'X-RapidAPI-Key': process.env.CRYPTO_RAPID_API_KEY,
+      'X-RapidAPI-Host': process.env.CRYPTO_RAPID_API_HOST,
+    },
+  }
+
+  const mockData = process.env.CRYPTO_RAPID_MOCK_API_DATA === 'true'
+  const resp = mockData ? apiCryptoHistory : await axios.request(options)
+  const serverCoins = mockData ? resp.data.history : resp?.data?.data?.history
+  res.status(200).json({ success: true, data: serverCoins })
+})
+
+//Geolocation
 
 exports.getGeolocationWithKey = asyncHandler(async (req, res) => {
   const { key, address } = req.params
--- routes/crypto.js
+++ routes/crypto.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { protect } = require('../middleware/auth')
-const { getCrypto } = require('../controllers/external')
+const { getCrypto, getCryptoHistory } = require('../controllers/external')
 
 router.route('/').get(protect, getCrypto)
+router.route('/history/:time').get(protect, getCryptoHistory)
 
 module.exports = router
