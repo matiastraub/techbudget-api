6ca505afe0ad2eb0454bca730eb7eadf9f137db4
Matias Traub <matias.traub@gmail.com>
2025-08-14 12:38:51 -0400

    Add lists , countries controllers routes

--- routes/countries.js
+++ routes/countries.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { protect } = require('../middleware/auth')
-const { getCountries } = require('../controllers/mysql/countries')
+const { getCountries } = require('../controllers/encuestas/countries')
 
 router.route('/').get(protect, getCountries)
 
