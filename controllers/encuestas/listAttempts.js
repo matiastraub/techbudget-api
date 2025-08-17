7a57144a4eb4f4db2ca16f90ebbe548a78550b27
Matias Traub <matias.traub@gmail.com>
2025-08-17 14:47:33 -0400

    Add GetListAttempt, getListAtttemptByUltravox, processList

--- controllers/encuestas/listAttempts.js
+++ controllers/encuestas/listAttempts.js
@@ -3,6 +3,54 @@ const ErrorResponse = require('../../utils/ErrorResponse')
 const asyncHandler = require('../../middleware/async')
 const pool = require('../../config/mysql')
 
+const DEFAULT_CAMPAIGN_ID = 1
+const DEFAULT_CHANNEL = 1
+
+// @desc    Get list attempt by Id
+// @route   GET /api/encuestas/list-attempts/:id
+// @access  Private
+exports.getListAtttempt = asyncHandler(async (req, res, next) => {
+  //ultravox_call_id
+  console.log('getListAttempt', req.params)
+  let query, params
+  query = `SELECT * FROM list_attempts WHERE id = ?`
+  params = [req.params.id]
+
+  const [rows] = await pool.query(query, params)
+
+  if (rows.length === 0) {
+    return next(new ErrorResponse('List attempt not found', 404))
+  }
+
+  res.status(200).json({
+    success: true,
+    count: rows.length,
+    data: rows[0],
+  })
+})
+
+// @desc    Get list attempt by Id or ultravox_call_id
+// @route   GET /api/encuestas/list-attempts/n8n/ultravox/:ultravox
+// @access  Private
+exports.getListAtttemptByUltravox = asyncHandler(async (req, res, next) => {
+  let query, params
+
+  query = `SELECT * FROM list_attempts WHERE ultravox_call_id = ?`
+  params = [req.params.ultravox]
+
+  const [rows] = await pool.query(query, params)
+
+  if (rows.length === 0) {
+    return next(new ErrorResponse('List attempt not found', 404))
+  }
+
+  res.status(200).json({
+    success: true,
+    count: rows.length,
+    data: rows[0],
+  })
+})
+
 exports.updateListAttemptStatusByUltravox = async (ultravoxCallId, status) => {
   const [existing] = await pool.query(
     'SELECT * FROM list_attempts WHERE ultravox_call_id = ?',
@@ -84,4 +132,35 @@ exports.updateListAttemptStatus = asyncHandler(async (req, res, next) => {
     success: true,
     data: updated,
   })
+})
+
+// @desc    Process list to communicate with the different channels
+// @route   POST /api/lists/process
+// @access  Private
+exports.processList = asyncHandler(async (req, res, next) => {
+  campaign_id = DEFAULT_CAMPAIGN_ID
+  channel_id = DEFAULT_CHANNEL
+
+  const listIdsToInsertQuery = `SELECT id FROM lists WHERE id NOT IN (SELECT list_id FROM list_attempts);`
+
+  const [resultIds] = await pool.query(listIdsToInsertQuery, [campaign_id])
+  const ids = resultIds.map((i) => i.id)
+  if (ids.length > 0) {
+    const values = ids.map((id) => [
+      id,
+      channel_id,
+      new Date(),
+      'pending',
+      null,
+    ])
+
+    const insertQuery = `INSERT INTO list_attempts (list_id, channel_id, attempt_time, status, notes)
+    VALUES ? ON DUPLICATE KEY UPDATE attempt_time = attempt_time`
+
+    const [resultInsert] = await pool.query(insertQuery, [values])
+    const { insertId } = resultInsert
+    res.status(200).json({ success: true, data: insertId })
+  } else {
+    res.status(404).json({ success: false, msg: 'Not Found' })
+  }
 })
