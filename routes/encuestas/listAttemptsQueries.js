26131606c75ef6db1edf5437b10f5fcb4a0f3423
Matias Traub <matias.traub@gmail.com>
2025-08-19 08:29:48 -0400

    Update list Attempts Statuts bulk route

--- routes/encuestas/listAttemptsQueries.js
+++ routes/encuestas/listAttemptsQueries.js
@@ -2,7 +2,14 @@ const express = require('express')
 const router = express.Router()
 const { apiAuth } = require('../../middleware/auth')
 const listAttemptsQueries = require('../../controllers/encuestas/listAttemptsQueries')
+const listAttemptsController = require('../../controllers/encuestas/listAttempts')
 
 router.get('/n8n/status', apiAuth, listAttemptsQueries.getListAttemptsStatus)
+
+router.patch(
+  '/n8n/update-lists',
+  apiAuth,
+  listAttemptsController.updateListAttemptsStatusBulk
+)
 
 module.exports = router
