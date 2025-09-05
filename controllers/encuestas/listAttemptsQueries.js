93da82d33142025f96edd00b4389a4db6b0c91e2
Matias Traub <matias.traub@gmail.com>
2025-09-05 12:16:00 -0400

    Get list channel phoen by campaign, route, controller

--- controllers/encuestas/listAttemptsQueries.js
+++ controllers/encuestas/listAttemptsQueries.js
@@ -27,6 +27,41 @@ exports.getListsChannelPhone = asyncHandler(async (req, res, next) => {
   })
 })
 
+exports.getListsChannelPhoneByCampaignRequest = async (campaignId) => {
+  try {
+    const query = `SELECT l.phone, la.id, la.list_id, la.status,la.ultravox_call_id,us.candidate_id,
+                CONCAT(ca.name," ",ca.lastname) AS candidate,ca.name AS candidate_name,
+                ca.lastname AS candidate_lastname,
+                m.name AS municipality,r.name AS region,r.code AS \`region_code\`, d.id AS \`district\`,
+                us.created,us.joined, us.ended
+                FROM list_attempts la
+                INNER JOIN lists l ON l.id = la.list_id
+                INNER JOIN municipalities m ON m.id = l.municipality_id
+                INNER JOIN districts d ON d.id = m.district_id
+                INNER JOIN regions r ON r.id = m.region_id
+                LEFT JOIN ultravox_sessions us ON us.ultravox_call_id = la.ultravox_call_id
+                LEFT JOIN candidates ca ON ca.id = us.candidate_id
+                WHERE l.campaign_id = ?`
+    const [rows] = await pool.query(query, [campaignId])
+    return rows
+  } catch (error) {
+    console.error('DB Error:', error.message)
+    return []
+  }
+}
+
+exports.getListsChannelPhoneByCampaign = asyncHandler(
+  async (req, res, next) => {
+    const { id } = req.params
+    const rows = await exports.getListsChannelPhoneByCampaignRequest(id)
+    res.status(200).json({
+      success: true,
+      count: rows.length,
+      data: rows,
+    })
+  }
+)
+
 const getAllListAttemptsRequestByStatus = async (status) => {
   let query = `SELECT ca.id, c.phone,  m.name AS municipality,ch.name AS channel,
       ca.status, ca.attempt_time AS created,ca.ultravox_call_id AS ultravoxCallId
